30481bf4bb0be3d65978fae5f66d1f4bee11361e
Mihai Sebea <mihai.sebea@gameloft.com>
2019-03-21 10:21:43 +0100

    ! adding fbuild

---
+++ setup_worker.bat
@@ -0,0 +1,3 @@
+@echo off
+setx FASTBUILD_BROKERAGE_PATH \\barwks0056\fastbuild
+setx FASTBUILD_CACHE_PATH \\barwks0056\fastbuild\\cache
---
+++ worker_on_startup.bat
@@ -0,0 +1,2 @@
+echo %~dp0\FBuildWorker.exe > start_fbuild.bat
+copy start_fbuild.bat "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
